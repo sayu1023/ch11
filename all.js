b4f3a48e565e6d0fe5f54dbcd77ff33f3d3a39e9
sayu1023 <133659800+sayu1023@users.noreply.github.com>
2023-12-06 15:45:21 +0800

    first commit

---
+++ all.js
@@ -0,0 +1,21 @@
+// 11-01 範例
+// document.write("Hello1129, JavaScript!");
+
+
+//11-02 範例
+// var myName = "David";	//宣告姓名 maName字串
+// 	var myAge = 25;			//宣告年齡 myAge 數值
+// 	var myHeight = 180, myWeight = 75;	//宣告身高 myHeight 數值
+//     //體重 myWeight 體重  數值
+
+
+//     document.write("下午好，我是" + myAge +",<br/>");
+
+// 	document.write("大家好，我是" + myName + ",<br/>");
+// 	document.write("身高" + myHeight +"公分，體重" + myWeight + "公斤，<br/>");
+// 	document.write("今年" + myAge + "歲。");
+
+//11-03 範例
+var a = prompt("請輸入數字2", "0"); //宣告一個 a變數 提示()
+if (a > 0) document.write("您輸入的值是正數");
+//假如 a > 0 然後(then) 印出 "字串內容"
